96af464e22527b97b6b971470bd036b6932bffc2
SouthFox <master@southfox.me>
2024-11-02 23:17:33 +0800

    feat: add gen-update-list

--- game.scm
+++ game.scm
@@ -198,6 +198,21 @@
         (find-string-list-len-iter (cdr string-list) (+ (string-length (car (last-pair (car string-list)))) offset) )))
   (find-string-list-len-iter string-list 0))
 
+(define (gen-update-list string-length x y len)
+  (define (up-iter string-length x y llen part)
+    (if (= 0 llen)
+        part
+        (up-iter string-length
+                   x
+                   (+ 1 y)
+                   (- llen 1)
+                   (append part
+                           (list (list
+                                  (+ (* y string-length) x )
+                                  (+ len (+ (* y string-length) x))
+                                  ))))))
+  (up-iter (+ 1 string-length) x y len '()))
+
 ;; Main
 (set! *template* template-task)
 
